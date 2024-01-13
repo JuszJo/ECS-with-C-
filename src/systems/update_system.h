735ff4cc8735bcc89bc7498794786754a18e44b8
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2024-01-13 23:11:19 +0100

    add update system

---
+++ src/systems/update_system.h
@@ -0,0 +1,21 @@
+#ifndef UPDATE_SYSTEM_H
+#define UPDATE_SYSTEM_H
+
+#include "../entityV2.h"
+
+class UpdateSystem {
+    public:
+        int currentIndex = 0;
+
+        UpdateSystem() {}
+
+        void update(EntityV2** entityPointer, int arraySize) {
+            for(int i = 0; i < arraySize; ++i) {
+                if(entityPointer[i] -> active) {
+                    entityPointer[i] -> update();
+                }
+            }
+        }
+};
+
+#endif
