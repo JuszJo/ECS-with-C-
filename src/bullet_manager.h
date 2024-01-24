db31648cfb2a2ced13b8c4f7dec796456d6c7ad3
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2024-01-24 21:11:15 +0100

    remove comments

--- src/bullet_manager.h
+++ src/bullet_manager.h
@@ -7,23 +7,6 @@ class BulletManager {
     public:
         BulletManager() {}
 
-        /* static void createBullet(Shader* shader, float position_x, float position_y, bool reverseDirection = false) {
-            // std::cout << "active bullets: " << activeBullets << std::endl;
-            if(activeBullets < maxActiveBullets) {
-                Bullet* newBullet = new Bullet(shader, 0.0f, 0.0f, 10.0f, 10.0f, currentIndex);
-
-                newBullet -> x = position_x;
-                newBullet -> y = position_y;
-
-                if(reverseDirection) newBullet -> acceleration *= -1.0f;
-
-                newBullet -> active = true;
-
-                addEntity(newBullet);
-
-                ++activeBullets;
-            }
-        } */
         static void createBullet(Shader* shader, float position_x, float position_y, bool reverseDirection = false) {
             // std::cout << "active bullets: " << activeBullets << std::endl;
             if(activeBullets < maxActiveBullets) {
--- src/enemy_manager.h
+++ src/enemy_manager.h
@@ -7,18 +7,6 @@ class EnemyManager {
     public:
         EnemyManager() {}
 
-        /* static void createEnemy(Shader* shader, float position_x, float position_y) {
-            Enemy* newEnemy = new Enemy(shader, 0.0f, 0.0f, 50.0f, 50.0f, currentIndex);
-
-            newEnemy -> x = position_x;
-            newEnemy -> y = position_y;
-
-            newEnemy -> active = true;
-
-            addEntity(newEnemy);
-
-            ++enemyCount;
-        } */
         static void createEnemy(Shader* shader, float position_x, float position_y) {
             Enemy* newEnemy = new Enemy(shader, 0.0f, 0.0f, 50.0f, 50.0f);
 
@@ -38,13 +26,6 @@ class EnemyManager {
             }
         }
 
-        /* static void reduceMaxShootBuffer() {
-            for(int i = 0; i < size; ++i) {
-                if(entityList[i] -> active && entityList[i] -> name == (char*)"enemy") {
-                    entityList[i] -> performAction((char*)"reduce_max_shoot_buffer");
-                }
-            }
-        } */
         static void reduceMaxShootBuffer() {
             for(int i = 0; i < entityManager.entity_list.size(); ++i) {
                 if(entityManager.entity_list[i]->active && entityManager.entity_list[i]->name == (char*)"enemy") {
